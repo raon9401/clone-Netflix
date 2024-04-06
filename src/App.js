f629ba5a39f2c73e734ae433c88f1b30a8f301d8
raon9401 <raon9401@naver.com>
2024-04-07 00:13:18 +0900

    Style: nav style add

--- src/App.js
+++ src/App.js
@@ -4,19 +4,27 @@ import AppLayout from "./layout/AppLayout";
 import HomePage from "./pages/Home/HomePage";
 import MoviePage from "./pages/Movies/MoviePage";
 import MovieDetailPage from "./pages/MovieDetail/MovieDetailPage";
+import NotFoundPage from "./pages/NotFound/NotFoundPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // 홈페이지 : '/'
 // 영화 리스트 : '/movies'
 // 영화 상세 페이지 : '/movies/:id'
+// 추천 영화 : '/movies/:id/recommandation'
+// 리뷰 : '/movies/:id/reviews'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<AppLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="/movies" element={<MoviePage />} />
-        <Route path="/movies/:id" element={<MovieDetailPage />} />
+        <Route path="/movies">
+          <Route index element={<MoviePage />} />
+          <Route path=":id" element={<MovieDetailPage />} />
+        </Route>
       </Route>
+
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,8 +1,49 @@
+import "./AppLayout.style.css";
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {Outlet,useNavigate } from 'react-router-dom'
+
 
 const AppLayout = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>NavBar</div>
+    <div>
+      <Navbar expand="lg" className="bg-body-black">
+        <Container fluid="lg">
+          <Navbar.Brand onClick={() => navigate('/')}>
+            <img
+              style={{ maxHeight: '25px' , cursor:"pointer"}}
+              src="./image/netflix_logo.png" alt="logo"/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: '100px' }}
+              navbarScroll
+            >
+              <Nav.Link style={{ color: "white" }} onClick={() => navigate('/')}>Home</Nav.Link>
+              <Nav.Link style={{ color: "white" }} onClick={() => navigate('/movies')}>Movies</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-danger">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet/>
+    </div>
   )
 }
 
