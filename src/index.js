fcba7ce9cd2939a3c631eebf759d41906c550a84
raon9401 <raon9401@naver.com>
2024-04-07 16:14:36 +0900

    Feat: React query and axios setting

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const queryClient = new QueryClient();
+
 root.render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </BrowserRouter>
 );
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-const api = axios.create({
+export const api = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
-    Aaccept: "application/json",
+    Accept: "application/json",
     Authorization: `Bearer ${API_KEY}`,
   },
 });
