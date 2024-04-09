62dc4893a4da7c5deb9cb2d46fced0443c5eac09
raon9401 <raon9401@naver.com>
2024-04-09 23:15:54 +0900

    Feat: hook add

--- src/hooks/useMovieGenre.js
+++ src/hooks/useMovieGenre.js
@@ -1,26 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { api } from "../utils/api";
 
-const fetchPopularMovies = () => {
-  return api.get(`/movie/popular`);
-};
-
 const fetchGenresMovieList = () => {
   return api.get(`/genre/movie/list`);
-};
-
-export const usePopularMoviesQuery = () => {
-  return useQuery({
-    queryKey: ["movie-popular"],
-    queryFn: fetchPopularMovies,
-    select: (result) => result.data,
-  });
 };
 
 export const useGenresQuery = () => {
   return useQuery({
     queryKey: ["movie-genres"],
     queryFn: fetchGenresMovieList,
-    select: (result) => result.data,
+    select: (result) => result.data.genres,
+    staleTime: 300000,
   });
 };
