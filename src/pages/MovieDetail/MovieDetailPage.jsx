b22c68a5a09a37bec6388cac4d1a4a7220fde250
raon9401 <raon9401@naver.com>
2024-04-13 13:06:05 +0900

    Feat: Movie detail hook add

--- src/pages/MovieDetail/MovieDetailPage.jsx
+++ src/pages/MovieDetail/MovieDetailPage.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useMovieDetailQuery } from '../../hooks/useMovieDetail';
+import Spinner from 'react-bootstrap/Spinner';
+import Alert from "react-bootstrap/Alert"
 
 const MovieDetailPage = () => {
   const { id } = useParams();
   console.log(id);
+  const { data, isLoading, isError, error } = useMovieDetailQuery({id});
+  console.log(data);
+
+  if(isLoading) {
+    <div className="spinner-area">
+      <Spinner
+        animation='border'
+        variant="danger"
+        style={{width: "5rem", height:"5rem"}}
+        />
+    </div>
+  }
+  if(isError){
+      <Alert variant="danger">{error.message}</Alert>
+  }
+
   return (
-    <div>MovieDetailPage</div>
+    <div>MovieDetailPage{id}</div>
   )
 }
 
